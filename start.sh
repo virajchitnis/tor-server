bbce0600058ae40430b91bd497fa8a1de014bd56
Viraj Chitnis <chitnisviraj@gmail.com>
2022-07-20 20:12:27 -0400

    Automatically create config/lib directory and set required permissions

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-docker run --detach --restart always --name tor-server -v $(pwd)/config/etc:/etc/tor:ro -v $(pwd)/config/lib:/var/lib/tor:rw --publish 9050:9050 virajchitnis/tor-server
+LIB_DIR=$(pwd)/config/lib
+CONTAINER_NAME=tor-server
+HOST_PORT=9050
+
+# Create lib directory if it doesnt exist
+if [ ! -d "$LIB_DIR" ]; then
+	mkdir $LIB_DIR
+fi
+
+# Set required permissions for lib directory
+sudo chown -R 100:100 $LIB_DIR
+
+# Start docker container
+docker run --detach --restart always --name $CONTAINER_NAME -v $(pwd)/config/etc:/etc/tor:ro -v $(pwd)/config/lib:/var/lib/tor:rw --publish $HOST_PORT:9050 virajchitnis/tor-server
